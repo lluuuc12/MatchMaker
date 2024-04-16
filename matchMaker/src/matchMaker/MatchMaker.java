c818379aab550daf457853c4056af1a45b441783
lluuuc12 <puertesarrue@gmail.com>
2024-04-16 08:51:59 +0200

    Changing name
    Signed-off-by: lluuuc12 <puertesarrue@gmail.com>

--- matchMaker/src/matchMaker/MatchMaker.java
+++ matchMaker/src/matchMaker/MatchMaker.java
@@ -1,11 +1,15 @@
-package affinityConnect;
+package matchMaker;
 
 import java.awt.EventQueue;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Properties;
 
@@ -24,11 +28,12 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import java.awt.Color;
 
 class ConnectionSingleton {
 	private static Connection con;
 	public static Connection getConnection() throws SQLException {
-        String url = "jdbc:postgresql://127.0.0.1:5433/affinityconnect";
+        String url = "jdbc:postgresql://127.0.0.1:5433/matchmaker";
 		String user = "alumno";
 		String password = "alumno";
 		if (con == null || con.isClosed()) {
@@ -57,11 +62,11 @@ class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
     }
 }
 
-public class AffinityConnect {
+public class MatchMaker {
 
-	private JFrame frmAffinityConnect;
+	private JFrame frmMatchMaker;
 	private Connection con;
-	private JTextField textFieldName;
+	private JTextField textFieldFirstName;
 	private JTextField textFieldLastName;
 	private JTextField textFieldPicText;
 	private JDatePickerImpl datePicker;
@@ -73,8 +78,8 @@ public class AffinityConnect {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AffinityConnect window = new AffinityConnect();
-					window.frmAffinityConnect.setVisible(true);
+					MatchMaker window = new MatchMaker();
+					window.frmMatchMaker.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -85,7 +90,7 @@ public class AffinityConnect {
 	/**
 	 * Create the application.
 	 */
-	public AffinityConnect() {
+	public MatchMaker() {
 		initialize();
 	}
 
@@ -93,60 +98,59 @@ public class AffinityConnect {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmAffinityConnect = new JFrame();
-		frmAffinityConnect.setTitle("Affinity Connect");
-		frmAffinityConnect.setBounds(100, 100, 684, 776);
-		frmAffinityConnect.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmAffinityConnect.getContentPane().setLayout(null);
+		frmMatchMaker = new JFrame();
+		frmMatchMaker.setBackground(new Color(240, 240, 240));
+		frmMatchMaker.setTitle("MatchMaker");
+		frmMatchMaker.setBounds(100, 100, 684, 776);
+		frmMatchMaker.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMatchMaker.getContentPane().setLayout(null);
 		
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(30, 33, 54, 14);
-		frmAffinityConnect.getContentPane().add(lblName);
+		frmMatchMaker.getContentPane().add(lblName);
 		
-		textFieldName = new JTextField();
-		textFieldName.setBounds(75, 30, 86, 20);
-		frmAffinityConnect.getContentPane().add(textFieldName);
-		textFieldName.setColumns(10);
+		textFieldFirstName = new JTextField();
+		textFieldFirstName.setBounds(75, 30, 86, 20);
+		frmMatchMaker.getContentPane().add(textFieldFirstName);
+		textFieldFirstName.setColumns(10);
 		
 		JLabel lblLastName = new JLabel("Last Name:");
 		lblLastName.setBounds(182, 33, 76, 14);
-		frmAffinityConnect.getContentPane().add(lblLastName);
+		frmMatchMaker.getContentPane().add(lblLastName);
 		
 		textFieldLastName = new JTextField();
 		textFieldLastName.setBounds(250, 30, 149, 20);
-		frmAffinityConnect.getContentPane().add(textFieldLastName);
+		frmMatchMaker.getContentPane().add(textFieldLastName);
 		textFieldLastName.setColumns(10);
 		
 		JLabel lblDateOfBirth = new JLabel("Date of birth:");
 		lblDateOfBirth.setBounds(409, 33, 76, 14);
-		frmAffinityConnect.getContentPane().add(lblDateOfBirth);
+		frmMatchMaker.getContentPane().add(lblDateOfBirth);
+		
+		UtilDateModel model = new UtilDateModel();
+		Properties properties = new Properties();
+		properties.put("text.today", "Today");
+        properties.put("text.month", "Month");
+        properties.put("text.year", "Year");	
+        JDatePanelImpl datePanel = new JDatePanelImpl(model, properties);
+        datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+        datePicker.setBounds(495, 30, 150, 25);
+        frmMatchMaker.getContentPane().add(datePicker);
 		
 		JLabel lblImage = new JLabel("Image:");
 		lblImage.setBounds(30, 69, 54, 14);
-		frmAffinityConnect.getContentPane().add(lblImage);
+		frmMatchMaker.getContentPane().add(lblImage);
 		
 		textFieldPicText = new JTextField();
 		textFieldPicText.setBounds(85, 66, 273, 20);
-		frmAffinityConnect.getContentPane().add(textFieldPicText);
+		frmMatchMaker.getContentPane().add(textFieldPicText);
 		textFieldPicText.setColumns(10);
-		
-		JButton btnAddPerson = new JButton("Add Person");
-		btnAddPerson.setBounds(83, 476, 110, 23);
-		frmAffinityConnect.getContentPane().add(btnAddPerson);
-		
-		JButton btnUpdatePerson = new JButton("Update Person");
-		btnUpdatePerson.setBounds(276, 476, 110, 23);
-		frmAffinityConnect.getContentPane().add(btnUpdatePerson);
-		
-		JButton btnDeletePerson = new JButton("Delete Person");
-		btnDeletePerson.setBounds(469, 476, 116, 23);
-		frmAffinityConnect.getContentPane().add(btnDeletePerson);
 		
 		JLabel lblHobbies = new JLabel("Hobbies");
 		lblHobbies.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHobbies.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblHobbies.setBounds(301, 97, 65, 14);
-		frmAffinityConnect.getContentPane().add(lblHobbies);
+		frmMatchMaker.getContentPane().add(lblHobbies);
 		
 		JButton btnNewButton = new JButton("Select Image");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -159,17 +163,44 @@ public class AffinityConnect {
 			}
 		});
 		btnNewButton.setBounds(368, 65, 107, 23);
-		frmAffinityConnect.getContentPane().add(btnNewButton);
+		frmMatchMaker.getContentPane().add(btnNewButton);
 		
-		UtilDateModel model = new UtilDateModel();
-		Properties properties = new Properties();
-		properties.put("text.today", "Today");
-        properties.put("text.month", "Month");
-        properties.put("text.year", "Year");	
-        JDatePanelImpl datePanel = new JDatePanelImpl(model, properties);
-        datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
-        datePicker.setBounds(495, 30, 150, 25);
-        frmAffinityConnect.getContentPane().add(datePicker);
+		JButton btnAddPerson = new JButton("Add Person");
+		btnAddPerson.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					con = ConnectionSingleton.getConnection();
+		            java.util.Date selectedDate = (java.util.Date) datePicker.getModel().getValue();
+		            java.sql.Date birthDate = new java.sql.Date(selectedDate.getTime());
+		            LocalDate dob = selectedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		            LocalDate today = LocalDate.now();
+		            int age = Period.between(dob, today).getYears();
+		            String photoPath = textFieldPicText.getText();
+					PreparedStatement ins_pstmt = con.prepareStatement("INSERT INTO persons (first_name, last_name, birth_date, age, photo) VALUES (?, ?, ?, ?, ?)");
+							ins_pstmt.setString(1, textFieldFirstName.getText());
+							ins_pstmt.setString(2, textFieldLastName.getText());							
+							ins_pstmt.setDate(3, birthDate);
+							ins_pstmt.setInt(4, age);
+							ins_pstmt.setString(5, photoPath);
+							ins_pstmt.executeUpdate();
+							ins_pstmt.close();
+				} catch (SQLException e) {
+					System.err.println(e.getMessage());
+					e.getErrorCode();
+					e.printStackTrace();
+				}
+			}
+		});
+		btnAddPerson.setBounds(83, 391, 110, 23);
+		frmMatchMaker.getContentPane().add(btnAddPerson);
+		
+		JButton btnUpdatePerson = new JButton("Update Person");
+		btnUpdatePerson.setBounds(276, 391, 110, 23);
+		frmMatchMaker.getContentPane().add(btnUpdatePerson);
+		
+		JButton btnDeletePerson = new JButton("Delete Person");
+		btnDeletePerson.setBounds(469, 391, 116, 23);
+		frmMatchMaker.getContentPane().add(btnDeletePerson);
         
 		try {
 			con = ConnectionSingleton.getConnection();
